e0a60d462b27406448aa7547d7fa63b20679ea54
jrsmth <jamesrichardsmith97@gmail.com>
2025-05-29 00:16:19 +0100

    [demo] font and zoom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,8 @@ export class AppComponent implements AfterViewInit {
   tree: any;
   config: any;
 
+  // TODO :: actions (define zoom zones?), mobile friendly
+
   constructor(private configService: ConfigService) {}
 
   async ngAfterViewInit() {
@@ -46,22 +48,22 @@ export class AppComponent implements AfterViewInit {
         vars.groupColor = params.group.color;
         vars.labelColor = "auto";
       },
-      groupFillType: "plain",
+      groupFillType: "gradient",
+      groupLabelFontFamily: "inter",
 
       // mobile optimisation
-      // relaxationVisible: false,
-      // relaxationQualityThreshold: 5,
-      // rolloutDuration: 0,
-      // pullbackDuration: 0,
-      // finalCompleteDrawMaxDuration: 50,
-      // finalIncrementalDrawMaxDuration: 20,
-      // interactionHandler: "hammerjs"
+      relaxationVisible: false,
+      relaxationQualityThreshold: 5,
+      rolloutDuration: 0,
+      pullbackDuration: 0,
+      finalCompleteDrawMaxDuration: 50,
+      finalIncrementalDrawMaxDuration: 20,
+      interactionHandler: "hammerjs",
 
       // Roll out in groups
       rolloutMethod: "groups",
 
       onRolloutComplete: function () {
-        console.log("hit!");
         tree.set("open", { open: false, groups: [...divisions, ...books] });
       },
       onGroupClick: function (event: any) {
@@ -78,6 +80,9 @@ export class AppComponent implements AfterViewInit {
       onGroupMouseWheel: function (event: any) {
         if (event.delta < 0) {
           tree.set("open", { groups: [...books, ...divisions], open: false, keepPrevious: true});
+        }
+        if (event.delta > 0) {
+            tree.open(event.group.id);
         }
       }
     });
@@ -97,7 +102,7 @@ export class AppComponent implements AfterViewInit {
 
     for (const test of this.config.testaments) {
       testaments.push({
-        id: test.name,
+        id: test.name.toLowerCase(),
         groups: this.getDivisions(test.divisions),
         label: test.name,
         open: true,
@@ -217,55 +222,60 @@ export class AppComponent implements AfterViewInit {
     return book;
   }
 
+  reset() {
+    this.tree.reset();
+    this.tree.set("open", { groups: ["old", "new"], open: true});
+  }
+
   getColour(book: string): any {
     const colour: any = {
       "GEN": "#36ABFF",
       "EXO": "#36ABFF",
-      "LEV": "#36ABFF",
-      "NUM": "#36ABFF",
-      "DEU": "#36ABFF",
+      "LEV": "#1591ea",
+      "NUM": "#317db5",
+      "DEU": "#4aa5e6",
 
       "JOS": "#8967F6",
-      "JDG": "#8967F6",
-      "RUT": "#8967F6",
-      "1SA": "#8967F6",
-      "2SA": "#8967F6",
-      "1KI": "#8967F6",
-      "2KI": "#8967F6",
-      "1CH": "#8967F6",
-      "2CH": "#8967F6",
-      "EZR": "#8967F6",
-      "NEH": "#8967F6",
-      "EST": "#8967F6",
+      "JDG": "#6c4ad8",
+      "RUT": "#6339ea",
+      "1SA": "#805af8",
+      "2SA": "#8d6ef1",
+      "1KI": "#6134f3",
+      "2KI": "#6644d3",
+      "1CH": "#7352dd",
+      "2CH": "#7b57f1",
+      "EZR": "#7756e1",
+      "NEH": "#6d4dd5",
+      "EST": "#7158c1",
 
       "JOB": "#C54A84",
       "PSA": "#C54A84",
-      "PRO": "#C54A84",
-      "ECC": "#C54A84",
-      "SNG": "#C54A84",
+      "PRO": "#e64993",
+      "ECC": "#c52b73",
+      "SNG": "#ef4a98",
 
-      "ISA": "#4DBA7E",
+      "ISA": "#58da93",
       "JER": "#4DBA7E",
-      "LAM": "#4DBA7E",
-      "EZK": "#4DBA7E",
-      "DAN": "#4DBA7E",
+      "LAM": "#2bca73",
+      "EZK": "#199a53",
+      "DAN": "#3c9f68",
 
       "HOS": "#D0B42B",
       "JOL": "#D0B42B",
       "AMO": "#D0B42B",
       "OBA": "#D0B42B",
-      "JON": "#D0B42B",
-      "MIC": "#D0B42B",
-      "NAM": "#D0B42B",
-      "HAB": "#D0B42B",
-      "ZEP": "#D0B42B",
+      "JON": "#cdaf1c",
+      "MIC": "#bda320",
+      "NAM": "#bca536",
+      "HAB": "#cdaf1c",
+      "ZEP": "#c5a81c",
       "HAG": "#D0B42B",
       "ZEC": "#D0B42B",
-      "MAL": "#D0B42B",
+      "MAL": "#caad1e",
 
       "MAT": "#D0B42B",
-      "MRK": "#D0B42B",
-      "LUK": "#D0B42B",
+      "MRK": "#c5ad3c",
+      "LUK": "#cdaf1c",
       "JHN": "#D0B42B",
 
       "ACT": "#4DBA7E",
@@ -274,24 +284,24 @@ export class AppComponent implements AfterViewInit {
       "1CO": "#36ABFF",
       "2CO": "#36ABFF",
       "GAL": "#36ABFF",
-      "EPH": "#36ABFF",
-      "PHP": "#36ABFF",
+      "EPH": "#1a74b5",
+      "PHP": "#3ca0e8",
       "COL": "#36ABFF",
-      "1TH": "#36ABFF",
+      "1TH": "#2786ca",
       "2TH": "#36ABFF",
       "1TI": "#36ABFF",
-      "2TI": "#36ABFF",
-      "TIT": "#36ABFF",
+      "2TI": "#1e8edf",
+      "TIT": "#458dc1",
       "PHM": "#36ABFF",
-      "HEB": "#36ABFF",
+      "HEB": "#2190df",
 
       "JAS": "#C54A84",
       "1PE": "#C54A84",
-      "2PE": "#C54A84",
-      "1JN": "#C54A84",
-      "2JN": "#C54A84",
-      "3JN": "#C54A84",
-      "JUD": "#C54A84",
+      "2PE": "#e15094",
+      "1JN": "#c81f6f",
+      "2JN": "#b85684",
+      "3JN": "#e12b81",
+      "JUD": "#ea4b96",
 
       "REV": "#8967F6"
     }
